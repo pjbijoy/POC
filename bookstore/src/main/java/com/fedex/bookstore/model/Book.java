c64d2d3062aa798c942656a2bc4e8d1f8c9a6843
Jayandan B. Pazhayidathu <pjbijoy@10.0.1.27>
2016-08-21 21:55:32 -0500

    Changes for mongod

---
+++ bookstore/src/main/java/com/fedex/bookstore/model/Book.java
@@ -0,0 +1,49 @@
+package com.fedex.bookstore.model;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.PersistenceConstructor;
+
+public class Book {
+	private static AtomicLong COUNTER = new AtomicLong(0L);
+	@Id private String id;
+	private String bookName;
+	private String authorName;
+	private String bookType;
+	
+	@PersistenceConstructor
+	public Book() {
+		this.id = String.valueOf(COUNTER.incrementAndGet());
+	}
+	
+	public String getBookName() {
+		return bookName;
+	}
+	
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+	
+	public String getAuthorName() {
+		return authorName;
+	}
+	
+	public void setAuthorName(String authorName) {
+		this.authorName = authorName;
+	}
+	
+	public String getBookType() {
+		return bookType;
+	}
+	
+	public void setBookType(String bookType) {
+		this.bookType = bookType;
+	}
+
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", bookName=" + bookName + ", authorName=" + authorName + ", bookType=" + bookType
+				+ "]";
+	}
+}
---
+++ bookstore/src/main/java/com/fedex/bookstore/repository/BookstoreRepository.java
@@ -0,0 +1,12 @@
+package com.fedex.bookstore.repository;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import com.fedex.bookstore.model.Book;
+
+public interface BookstoreRepository extends MongoRepository<Book, String> {
+	public Book findByBookName(String bookName);
+	public List<Book> findByAuthorName(String authorName);
+	public List<Book> findByBookType(String bookType);
+}
