d54f82628e5f2ef0703b4aa32ad6ec91794a6c54
Jayandan B. Pazhayidathu <pjbijoy@10.0.1.27>
2016-08-21 21:54:14 -0500

    Changes related to MongoDB

--- bookstore/src/main/java/com/fedex/bookstore/BookstoreApplication.java
+++ bookstore/src/main/java/com/fedex/bookstore/BookstoreApplication.java
@@ -8,7 +8,6 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -19,21 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableEurekaClient
 public class BookstoreApplication {
 	static Logger logger = LoggerFactory.getLogger(BookstoreApplication.class);
-
-	@RequestMapping(value = "/recommended")
-	public String readingList() {
-		logger.info("Providing reading list from service");
-		try {
-			//If setting this more than the limit set in command properties of client, client 
-			//call will go to fall back
-			Thread.sleep(10);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		} 
-
-		return "Cloud Native Java from service (O'Reilly)";
-	}
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 		logger.info("Started Book Store Application");
